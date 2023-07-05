9719bbe5a283157f9065537fb771fc27a236ea51
JackSoer <kirillrotko1@gmail.com>
2023-07-05 19:22:13 +0300

    Remove for 7 days button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
     <nav className="nav">
       <div className="container nav__container">
         <LocationSection />
-        <Link to="/For7Days" className="nav__schedule-link">
+        {/* <Link to="/For7Days" className="nav__schedule-link">
           <img
             className="nav__schedule-link-icon"
             src="./images/icons/schedule.svg"
             alt=""
           />
-        </Link>
+        </Link> */}
       </div>
     </nav>
   );
